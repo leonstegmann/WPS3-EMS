0fc658ed8f1976e10a792d4b0eb7fb91374e2fb5
leonstegmann <leon.stegmann@tum.de>
2024-10-02 10:36:32 +0200

    FIX time colum read in from json file and replaced ',' values with '.'

--- API_service/api_fetch_Datasets.py
+++ API_service/api_fetch_Datasets.py
@@ -1,6 +1,7 @@
 # Packages
 import requests
 import csv
+import pandas as pd
 
 
 # %% API Request
@@ -52,40 +53,36 @@ def write_api_NordPoolGroup_data_to_csv(data_url, data_filename):
 
     # Extract the records from the API response
     data = response.json()
-    #print(result)
+   # print(data)
 
-    # Prepare a list to hold CSV rows
-    csv_rows = []
     if data:
-        # Extracting data from JSON and organizing it for CSV
-        for row in data['data']['Rows']:
-            for column in row['Columns']:
-                # Create a dictionary for each column
-                csv_row = {
-                    "Index": column['Index'],
-                    "Name": column['Name'],
-                    "Value": column['Value'],
-                    "IsValid": column['IsValid'],
-                    "IsOfficial": column['IsOfficial'],
-                    "DateTimeForData": column['DateTimeForData']
-                }
-                csv_rows.append(csv_row)
 
-        # Write to CSV file
-        with open(data_filename, mode='w', newline='') as csv_file:
-            fieldnames = csv_rows[0].keys()  # Get the field names from the first row
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        # Open a CSV file to write the output
+        with open(data_filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            # Write header row
+            writer.writerow(['Area', 'Value', 'StartTime', 'EndTime'])
 
-            writer.writeheader()  # Write the header
-            writer.writerows(csv_rows)  # Write the data rows
+            # Extract the values
+            rows = data['data']['Rows']
+            for row in rows:
+                start_time = row['StartTime']
+                end_time = row['EndTime']
+                for column in row['Columns']:
+                    area = column['Name']
+                    # Replace comma with dot in the value
+                    value = column['Value'].replace(',', '.')
+                    # Write each row to the CSV file
+                    writer.writerow([area, value, start_time, end_time])
+
 
         print(f"Data has been written to {data_filename}")
-        print("Total entries written: ", len(csv_rows))
+        print("Total entries written: ", len(pd.read_csv(data_filename)))
     else:
         print("No records found to write.")
 
 # FIT
-url = 'https://www.nordpoolgroup.com/api/marketdata/page/424408?currency=,,,EUR'
+url = 'https://www.nordpoolgroup.com/api/marketdata/page/424408?currency=,,DKK,EUR'
 filename = folderpath + 'FIT_data.csv'
 write_api_NordPoolGroup_data_to_csv(url, filename)
 
